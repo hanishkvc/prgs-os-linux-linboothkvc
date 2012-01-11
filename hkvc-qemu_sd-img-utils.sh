db2783670cfcbdee50abd223e80f84351e893acd
hkvc.india <hkvc.india@gmail.com>
2012-01-11 13:48:47 +0530

    hkvc-make.sh --------------- Added path for beagle android in office machine
    hkvc-qemu_sd-img-utils.h
    ---------------------------
    
    Added sudo for commands requiring root permissions. This was done so that one
    doesn't have to give/enable ALL ALL sudo permission to the user/developer
    account on the machine. Instead selective sudo permission can be given for
    commands used in the script with sudo like losetup kpartx mount mkfs.????
    
    Added a new special create command which doesn't run when all command is given.
    This helps create a SD Image and partition and format it as required.
    
    Split the previous mount into a new prepare and mount
    
    Also updated mount command to mount with developer uid passed, so that the
    developer can modify the mounted partitions.
    
    Added a new ANDPATH variable, so that one doesn't have to edit it in multiple
    locations.

--- hkvc-qemu_sd-img-utils.sh
+++ hkvc-qemu_sd-img-utils.sh
@@ -5,35 +5,55 @@
 # HKVC, GPL, Jan2012
 #
 
+ANDPATH=/hkvcwork/externel/rowboat/gingerbread-nondsp
+#ANDPATH=/hanishkvc/external/Android/rowboat-gingerbread
+
+
 if [[ $# < 2 ]]; then
 	echo "USAGE: $0 sd_image_raw mode"
-	echo "mode = run|mount|copy|umount|all"
+	echo "mode = prepare|mount|copy|umount|run|all"
+	echo "special mode = create"
 	exit
 fi
 
 df
 echo "Free loop device is "
-losetup -f
+sudo losetup -f
 read -p "Hope loop0 is free and your image $1 is not already mounted ..."
 
 mode=$2
 
-if [[ "$mode" == "mount" ]] || [[ "$mode" == "all" ]]; then
+if [[ "$mode" == "create" ]]; then
+	dd if=/dev/zero of=$1 bs=1K count=400K
+	sudo losetup /dev/loop0 $1
+	echo "Remember to create 2 partitions VFat and Ext4"
+	sudo fdisk /dev/loop0
+	sudo fdisk -l /dev/loop0
+	read -p "Are two partitions created"
+	sudo kpartx -a /dev/loop0
+	sudo mkfs.vfat -F 32 /dev/mapper/loop0p1
+	sudo mkfs.ext4 /dev/mapper/loop0p2
+fi
 
-	losetup /dev/loop0 $1
-	kpartx -a /dev/loop0
+if [[ "$mode" == "prepare" ]] || [[ "$mode" == "all" ]]; then
+	sudo losetup /dev/loop0 $1
+	sudo kpartx -a /dev/loop0
 	ls -l /dev/mapper/*
-	mount /dev/mapper/loop0p1 /mnt/d1
+	read -p "Hope you can see the partitions listed above..."
+fi
+
+if [[ "$mode" == "mount" ]] || [[ "$mode" == "all" ]]; then
+	sudo mount -o uid=hanishkvc /dev/mapper/loop0p1 /mnt/d1
 	df
 	read -p "The 1st partition should from the image $1 should be mounted ..."
 fi
 
 
 if [[ "$mode" == "copy" ]] || [[ "$mode" == "all" ]]; then
-	#cp /hanishkvc/external/Android/rowboat-gingerbread/x-loader/MLO /mnt/d1/
-	cp /hanishkvc/external/Android/rowboat-gingerbread/u-boot/u-boot.bin /mnt/d1/
-	cp /hanishkvc/external/Android/rowboat-gingerbread/kernel/arch/arm/boot/uImage /mnt/d1/
-	pushd /hanishkvc/external/Android/rowboat-gingerbread/out/target/product/beagleboard/root
+	cp $ANDPATH/x-loader/MLO /mnt/d1/
+	cp $ANDPATH/u-boot/u-boot.bin /mnt/d1/
+	cp $ANDPATH/kernel/arch/arm/boot/uImage /mnt/d1/
+	pushd $ANDPATH/out/target/product/beagleboard/root
 	echo "Update initrd system if required, else exit"
 	bash
 	find . | cpio -o -H newc | gzip > /tmp/initrd
@@ -45,9 +65,9 @@ if [[ "$mode" == "copy" ]] || [[ "$mode" == "all" ]]; then
 fi
 
 if [[ "$mode" == "umount" ]] || [[ "$mode" == "all" ]]; then
-	umount /mnt/d1
-	kpartx -d /dev/loop0
-	losetup -d /dev/loop0
+	sudo umount /mnt/d1
+	sudo kpartx -d /dev/loop0
+	sudo losetup -d /dev/loop0
 fi
 
 
